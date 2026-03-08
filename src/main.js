e98385731d34d765ef97b2ce140bdac393eeaff3
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-03-07 20:47:21 -0500

    Main logic flow

--- src/main.js
+++ src/main.js
@@ -1,24 +1,50 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import { getWeather } from "./api-calls";
+import "./display-weather.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const body = document.querySelector("body");
 
-setupCounter(document.querySelector('#counter'))
+// selecting the name field in the input form (so we don't have to declare a specific ID for that one input field)
+const locationInputField = document.querySelector("input[name='location']");
+// selecting the entire form
+const locationForm = document.querySelector("#location-form");
+
+// weather URL to build api call string
+let weatherUrl =
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+// leaving userInputCity null for the time being
+let userLocation = null;
+
+// api key to append to the end of the user input city
+let apiKey = "?key=QHJPA4XZ2MVSAD73HT7RRF533";
+
+// function to handle form submission
+async function submitForm(e) {
+  // preventing default behaviour of form after submitting
+  e.preventDefault();
+  // taking the users location in the locationInputField and then trimming any white space
+  userLocation = locationInputField.value.trim();
+  // guard clause for if no location was entered
+  if (!userLocation) {
+    throw new Error("No city entered");
+  }
+  // else log the string to the console
+  console.log("city entered is:", userLocation);
+  // using await to wait for getWeather to resolve
+  // storing the result in weatherData variable so we can access the full API response object
+  // and its properties
+  const weatherData = await getWeather(weatherUrl + userLocation + apiKey);
+  console.log(weatherData);
+  // TODO: create helper function to convert temp from farenheit to celsius
+  // NOTE: remove these two variables below in favor of having a function
+  // to extract the data from weatherData object
+  const temp = weatherData.currentConditions.temp;
+  const conditions = weatherData.currentConditions.conditions;
+  console.log(temp, conditions);
+  // TODO: call function from display-weather.js here to take the weather data above
+  // as an arguement
+}
+
+// adding event listener to the entire form in, listening for submit, if submit then use callback
+locationForm.addEventListener("submit", submitForm);
